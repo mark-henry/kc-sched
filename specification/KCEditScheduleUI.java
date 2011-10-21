f989859e022605a9f29b3a2600a79d7ff91fa919
boardsportsrule13@gmail.com <boardsportsrule13@gmail.com@5389afa3-3cff-bc22-2e1c-0e5405de9ceb>
2011-10-21 06:27:30 +0000

    updated KCEditScheduleUI.java

--- specification/KCEditScheduleUI.java
+++ specification/KCEditScheduleUI.java
@@ -1,21 +1,44 @@
 import java.util.*;
 public abstract class KCEditScheduleUI {
+
 	public abstract class KCComponents{
-	Collection<KCInstructor> instructors;
-	Collection<KCRoom> rooms;
-	Collection<KCCourse> courses;
+		/*data fields for schedule generation*/
+		Collection<KCInstructor> instructors;
+		Collection<KCRoom> rooms;
+		Collection<KCCourse> courses;
+		/*will be called in default constructor, will import all of the type that
+		are marked as "active" or "current"*/
+		public abstract int ImportAllCurrentType(void Type);
 	}
 	
 	public abstract class KCPreferences{
-	Collection<KCRule> rules;
+		/* preferences to be considered while generating schedules*/
+		Collection<KCRule> preferences;
+		/* verifies the program understands the rule as written by user*/
+		public abstract boolean IsRuleValid(KCRule rule);
 	}
 	
 	public abstract class KCConflicts{
-	Collection<KCRule> conflicts;
+		/* Settings to be followed when a conflict is being resolved by generation*/
+		Collection<KCRule> conflicts;
+		/* Verifies the program understands the rule as written by user */
+		public abstract boolean IsRuleValid(KCRule conflict);
 	}
 	
 	public abstract class KCGenerate{
-
+		/* generates a schedule based on the inputted instructors, rooms, courses,
+		preferences, and conflict resoultion settings */
+		public abstract KCSchedule Generate(Collection<KCInstructor> instructors,
+											Collection<KCRoom> rooms,
+											Collection<KCCourse> courses,
+											Collection<KCRule> conflicts,
+											Collection<KCRule> preferences);
+		/* deals with conflicting sections as defined in conflict settings*/ 
+		public abstract int DisplayConflict(Collection<KCSection> sections,Collection<KCRule> conflicts);
+		/* verifies validity of schedule. May be called in Generate function, or in
+		the edit schedule tab as well */
+		public abstract boolean IsScheduleValid(KCSchedule schedule);
+	
 	}
 	
 }
