e2542f817bb069f00c9031a285b4b7bccde753d5
TheRatcheteer@gmail.com <TheRatcheteer@gmail.com@5389afa3-3cff-bc22-2e1c-0e5405de9ceb>
2011-10-26 04:50:36 +0000

    My java file & updated spec link

---
+++ trunk/specification/KCEditTab.java
@@ -0,0 +1,38 @@
+/*
+ * KCEditTab.java
+ * Author: Mark Henry
+ * Date: 10-20-2011
+ * 
+ * This class represents what is seen when the 'Edit' tab is selected.
+ */
+
+import java.util.*;
+public abstract class KCEditTab {
+	Collection<Section> sections;
+	
+	// Adds a section to the grid
+	public abstract void addSection();
+	
+	// Pins a Section to prevent auto-scheduling
+	public abstract void pinSection();
+	
+	// Moves a section from one time to another
+	public abstract void moveSection();
+	
+	// Opens a section for editing
+	public abstract void editSection();
+	
+	// Displays/hides rollover info pane for a section
+	public abstract void displayRollover();
+	public abstract void hideRollover();
+	
+	// View controls
+	public abstract void zoomInVertical();
+	public abstract void zoomOutVertical();
+	public abstract void zoomInHorizontal();
+	public abstract void zoomOutHorizontal();
+	public abstract void scrollDown();
+	public abstract void scrollUp();
+	public abstract void scrollLeft();
+	public abstract void scrollRight();
+}
