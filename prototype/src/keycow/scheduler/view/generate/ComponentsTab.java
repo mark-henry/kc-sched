7ea58488061fe7279470190907ffa56b208bfef4
cjgaydosh@gmail.com <cjgaydosh@gmail.com@5389afa3-3cff-bc22-2e1c-0e5405de9ceb>
2011-11-20 06:12:58 +0000

    updated ComponentsTab.java

--- prototype/src/keycow/scheduler/view/generate/ComponentsTab.java
+++ prototype/src/keycow/scheduler/view/generate/ComponentsTab.java
@@ -5,4 +5,66 @@ import java.awt.*;
 
 public class ComponentsTab extends JPanel {
 
+   private static int rows = 35;
+
+   public ComponentsTab()
+   {
+      JPanel panel = new JPanel();
+      setLayout(new BoxLayout (this, BoxLayout.X_AXIS));
+      JLabel instructorsLabel = new JLabel("Instructors");
+      JLabel coursesLabel = new JLabel("Courses");
+      JLabel roomsLabel = new JLabel("Rooms");
+      
+      String [] options = {"+", "Import All Marked Current", "Select Individual"};
+      
+      JList instructorsList = new JList();
+      JScrollPane InstructorsScrollPane = new JScrollPane(instructorsList);
+      
+      instructorsList.setSelectionMode(ListSelectionModel.SINGLE_INTERVAL_SELECTION);
+      instructorsList.setLayoutOrientation(JList.VERTICAL);
+      instructorsList.setVisibleRowCount(rows);
+      instructorsList.setBackground (Color.blue);
+      instructorsList.setForeground (Color.blue);
+      
+      JComboBox instructorsBox = new JComboBox (options);
+
+      
+      JList coursesList = new JList();
+      JScrollPane CoursesScrollPane = new JScrollPane(coursesList);
+      
+      coursesList.setSelectionMode(ListSelectionModel.SINGLE_INTERVAL_SELECTION);
+      coursesList.setLayoutOrientation(JList.VERTICAL);
+      coursesList.setVisibleRowCount(rows);
+      coursesList.setBackground (Color.blue);
+      coursesList.setForeground (Color.blue);
+      
+      JComboBox coursesBox = new JComboBox (options);
+      
+      JList roomsList = new JList();
+      JScrollPane RoomsScrollPane = new JScrollPane(roomsList);
+      
+      roomsList.setSelectionMode(ListSelectionModel.SINGLE_INTERVAL_SELECTION);
+      roomsList.setLayoutOrientation(JList.VERTICAL);
+      roomsList.setVisibleRowCount(rows);
+      roomsList.setBackground (Color.blue);
+      roomsList.setForeground (Color.blue);
+      
+      JComboBox roomsBox = new JComboBox (options);
+      
+      
+/*      
+      panel.add(instructorsLabel);
+      panel.add(coursesLabel);
+      panel.add(roomsLabel);
+//*/
+      panel.add(InstructorsScrollPane);
+      panel.add(CoursesScrollPane);
+      panel.add(RoomsScrollPane);
+      panel.add(instructorsBox);
+      panel.add(coursesBox);
+      panel.add(roomsBox);
+      panel.setVisible(true);
+      
+      this.add(panel);
+   }
 }
