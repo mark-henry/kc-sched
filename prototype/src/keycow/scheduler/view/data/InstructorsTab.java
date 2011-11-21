f689ac785cd6a1d82c3e401b868b039ed982e38e
bradley.r.zeller@gmail.com <bradley.r.zeller@gmail.com@5389afa3-3cff-bc22-2e1c-0e5405de9ceb>
2011-11-21 01:07:12 +0000

    updated my gui

--- prototype/src/keycow/scheduler/view/data/InstructorsTab.java
+++ prototype/src/keycow/scheduler/view/data/InstructorsTab.java
@@ -6,47 +6,87 @@ import javax.swing.border.*;
 
 public class InstructorsTab extends JPanel {
    private JPanel instructor_list, instructor_details;
-   private JButton b1, b2;
    
    public InstructorsTab() {
+      createInstructorList();
+      createInstructorDetails();
+   }
+   
+   public void createInstructorList() {
+      
+      /*
+       * This is the left of the two main windows
+       */
       instructor_list = new JPanel();
       instructor_list.setPreferredSize(new Dimension(400, 650));
       instructor_list.setLayout(new BoxLayout(instructor_list, BoxLayout.Y_AXIS));
       instructor_list.setBorder(new TitledBorder("Instructor List"));
+      
+      /*
+       * Hard coded info
+       * Table holds all teacher info
+       */
       String columns[] = {"Last Name", "First Name"};
       Object data[][] = {{"Workman", "Julie"}, {"Fisher", "Gene"}, {"Bellardo", "John"}};
       JTable table = new JTable(data, columns);
       
+      /*
+       * JPanel to hold the add / delete button
+       */
       JPanel button_panel = new JPanel();
       button_panel.setLayout(new BoxLayout(button_panel, BoxLayout.X_AXIS));
       button_panel.add(new JButton("+"));
       button_panel.add(new JButton("-"));
       
+      /*
+       * add the table and button panel to the left window
+       */
       instructor_list.add(table);
       instructor_list.add(button_panel);
       
+      /*
+       * add the window to the main frame
+       */
+      
+      add(instructor_list);
+   }
+   
+   public void createInstructorDetails() {
       instructor_details = new JPanel();
       instructor_details.setPreferredSize(new Dimension(400, 650));
       instructor_details.setBorder(new TitledBorder("Instructor Details"));
-      instructor_details.add(new JLabel("First Name"));
-      instructor_details.add(new JTextField(20));
-      instructor_details.add(new JLabel("Last Name"));
-      instructor_details.add(new JTextField(20));
-      instructor_details.add(new JLabel("Phone Number"));
-      instructor_details.add(new JTextField(20));
-      instructor_details.add(new JLabel("Office"));
-      instructor_details.add(new JTextField(20));
-      instructor_details.add(new JLabel("Department"));
-      instructor_details.add(new JTextField(20));
-      instructor_details.add(new JLabel("Email Address"));
-      instructor_details.add(new JTextField(20));
-      instructor_details.add(new JLabel("WTUs"));
-      instructor_details.add(new JTextField(20));
-      instructor_details.add(new JButton("Commit"));
-      instructor_details.add(new JButton("Restore"));
       
-      add(instructor_list);
+      /*
+       * hard-coded labels
+       */
+      String labels[] = {"First Name: ", "Last Name: ", "Phone Number: ", "Office: ", "Department: ", 
+         "Email Address: ", "WTUs: "};
       
+      /*
+       * construct 6 JPanels and add each label / textfield to it
+       * then add the row to the right window
+       */
+      for (int i = 0; i < 6; i++) {
+         JPanel row = new JPanel();
+         row.setLayout(new BoxLayout(row, BoxLayout.X_AXIS));
+         row.add(new JLabel(labels[i]));
+         row.add(new JTextField(20));
+         instructor_details.add(row);
+      }
+ 
+      /*
+       * add buttons 
+       */
+       JPanel row = new JPanel();
+       row.setLayout(new BoxLayout(row, BoxLayout.X_AXIS));
+       row.add(new JButton("Commit"));
+       row.add(new JButton("Restore"));
+       instructor_details.add(row);
+
+       
+      /*
+       * add the right window to the main frame
+       */
       add(instructor_details);
    }
 }
