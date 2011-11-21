41b093574ea927eb8d75ae090dc8fdc8881faca6
cjgaydosh@gmail.com <cjgaydosh@gmail.com@5389afa3-3cff-bc22-2e1c-0e5405de9ceb>
2011-11-21 06:20:02 +0000

    updated with JML

--- specification/KCSched/Export.java
+++ specification/KCSched/Export.java
@@ -5,22 +5,22 @@ public abstract class Export {
 
 
    /*@ requires (* must be a valid schedule *)
-    *@ ensures (* must be a valid html file in the format that 
-       peopleSoft expects *)
+     @ ensures (* must be a valid html file in the format that 
+     @ peopleSoft expects *)
      @*/ 
    // exports to peopleSoft
    public abstract void exportToPeopleSoft(Schedule schedule);
    
    /*@ requires (* must be a valid schedule *)
-    *@ ensures (* must be a valid Excel file (.xls) in the format that 
-       peopleSoft expects *)
+     @ ensures (* must be a valid Excel file (.xls) in the format that 
+     @ peopleSoft expects *)
      @*/ 
    // creates an excel file of the schedule
    public abstract void exportToExcel(Schedule schedule);
    
    /*@ requires (* must be a valid schedule *)
-    *@ ensures (* must be a valid PDF file in the format that 
-       peopleSoft expects *)
+     @ ensures (* must be a valid PDF file in the format that 
+     @ peopleSoft expects *)
      @*/ 
    // creates a PDF of the schedule
    public abstract void exportToPDF(Schedule schedule);
