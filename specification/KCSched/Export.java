53fe01ed0138e4a063f9fadcf1b64c34df0ba0c9
cjgaydosh@gmail.com <cjgaydosh@gmail.com@5389afa3-3cff-bc22-2e1c-0e5405de9ceb>
2011-11-21 04:35:47 +0000

    update Export.java

--- specification/KCSched/Export.java
+++ specification/KCSched/Export.java
@@ -3,19 +3,26 @@ package KCSched;
 import java.util.*;
 public abstract class Export {
 
+
+   /*@ requires (* must be a valid schedule *)
+    *@ ensures (* must be a valid html file in the format that 
+       peopleSoft expects *)
+     @*/ 
    // exports to peopleSoft
    public abstract void exportToPeopleSoft(Schedule schedule);
    
+   /*@ requires (* must be a valid schedule *)
+    *@ ensures (* must be a valid Excel file (.xls) in the format that 
+       peopleSoft expects *)
+     @*/ 
    // creates an excel file of the schedule
    public abstract void exportToExcel(Schedule schedule);
    
+   /*@ requires (* must be a valid schedule *)
+    *@ ensures (* must be a valid PDF file in the format that 
+       peopleSoft expects *)
+     @*/ 
    // creates a PDF of the schedule
    public abstract void exportToPDF(Schedule schedule);
    
-   // change directories that file is exporting to.
-   public abstract void changeDirecttory();
-   
-   
-   
-
 }
