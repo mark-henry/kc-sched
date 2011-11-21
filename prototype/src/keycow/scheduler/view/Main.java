1ffe0b479a5e701635760cfebdafa3c87b38d244
nathaniel.chamness@gmail.com <nathaniel.chamness@gmail.com@5389afa3-3cff-bc22-2e1c-0e5405de9ceb>
2011-11-21 00:13:19 +0000

    fixed ui on os x

--- prototype/src/keycow/scheduler/view/Main.java
+++ prototype/src/keycow/scheduler/view/Main.java
@@ -7,6 +7,24 @@ public class Main {
      * Construct a SchedulerUI and make it visible
      */
     public static void main(String[] args) {
-        new SchedulerUI().setVisible(true);
+       try {
+          // Set cross-platform Java L&F (also called "Metal")
+          UIManager.setLookAndFeel(UIManager.getCrossPlatformLookAndFeelClassName());
+       } 
+       catch (UnsupportedLookAndFeelException e) {
+          // handle exception
+       }
+       catch (ClassNotFoundException e) {
+          // handle exception
+       }
+       catch (InstantiationException e) {
+          // handle exception
+       }
+       catch (IllegalAccessException e) {
+          // handle exception
+       } 
+       
+       new SchedulerUI().setVisible(true);
     }
+   
 }
