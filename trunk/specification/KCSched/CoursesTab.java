d3ca4253aeafb1ff21a3bb7bde595dce743e2cc2
nathaniel.chamness@gmail.com <nathaniel.chamness@gmail.com@5389afa3-3cff-bc22-2e1c-0e5405de9ceb>
2011-11-30 17:12:06 +0000

    updated

--- trunk/specification/KCSched/CoursesTab.java
+++ trunk/specification/KCSched/CoursesTab.java
@@ -12,10 +12,18 @@ package KCSched;
 
 import java.util.*;
 public abstract class CoursesTab {
-	Collection<Course> coursesList;
+	
+   /**
+    * The collection of all the courses already added
+    */
+   Collection<Course> coursesList;
+   
+   /**
+    * A Course holds all the information about a course
+    */
    Course newCourse;
 	
-	/*
+	/**
 	 * Triggered when a user clicks the "+" button on the 'Courses Added'
     * section of the view. This will then bring up a clear 'Course Information'
     * section in which you can enter course info into.
@@ -23,7 +31,7 @@ public abstract class CoursesTab {
 	public abstract void addCourse();
    
    
-   /*
+   /**
     * Triggered when a user clicks the "-" button on the 'Courses Added'
     * section of the view. This will then remove the highlighted course in 
     * 'Courses Added'.
@@ -31,14 +39,14 @@ public abstract class CoursesTab {
    public abstract void removeCourse();
    
    
-   /*
+   /**
     * Triggered when a user selects a course in the 'Courses Added' section of
     * the view. This will then fill all the fields of the 'Course Information'
     * section of the view with the course information.
     */
    public abstract void viewInfo();
    
-   /*
+   /**
     * Triggered when a user clicks the "Commit" button on the 'Course
     * Information' section of the view. This will create a new Course object,
     * add it to the Collection of Courses, and display it in the 'Courses
@@ -47,20 +55,18 @@ public abstract class CoursesTab {
    public abstract void commitInfo();
    
    
-   /*
+   /**
     * Triggered when a user clicks the "Restore" button on the 'Course
     * Information' section of the view. This will clear all of the info already
     * entered into the 'Course Information' section.
     */
    public abstract void restoreInfo();
 	
-	/*
-	 * This is triggered when a user clicks the column heading. 
-	 * For example, if the user clicks the column "course name," then
-	 * the list of courses becomes sorted by course name.
-	 */
-	//public abstract void sortColumn();
 	
+	/**
+    * Helper method used to get the collection of courses that have already
+    * been added.
+    */
 	public abstract Collection<Course> getCourses();
 	
 }
