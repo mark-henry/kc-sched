d191a8adaf06550875b9a8953bd7f27f4fbd86d3
cjgaydosh@gmail.com <cjgaydosh@gmail.com@5389afa3-3cff-bc22-2e1c-0e5405de9ceb>
2011-12-02 05:47:55 +0000

    updated GenerateSchedule.java and export.java w/ JML

--- trunk/specification/KCSched/Export.java
+++ trunk/specification/KCSched/Export.java
@@ -1,28 +1,48 @@
 package KCSched;
 
 import java.util.*;
+
+
+/**
+ * Export is where the user can create a file that is in a 
+ * user specified format.
+ */
 public abstract class Export {
 
-
+   /**
+    * Create a file in the format of html that conforms to the Peoplesoft standards.
+    * @param schedule a generated schedule.
+    * @param directory string that contains the directory that the file is put into.
+    */
    /*@ requires (* must be a valid schedule *)
      @ ensures (* must be a valid html file in the format that 
      @ peopleSoft expects *)
      @*/ 
    // exports to peopleSoft
-   public abstract void exportToPeopleSoft(Schedule schedule);
+   public abstract void exportToPeopleSoft(Schedule schedule, String directory);
    
+   /**
+    * Create a file in the format of html that conforms to the Peoplesoft standards.
+    * @param schedule a generated schedule.
+    * @param directory string that contains the directory that the file is put into.
+    */
    /*@ requires (* must be a valid schedule *)
      @ ensures (* must be a valid Excel file (.xls) in the format that 
      @ peopleSoft expects *)
      @*/ 
    // creates an excel file of the schedule
-   public abstract void exportToExcel(Schedule schedule);
+   public abstract void exportToExcel(Schedule schedule, String directory);
    
+   /**
+    * Create a file in the format of html that conforms to the Peoplesoft standards.
+    * @param schedule a generated schedule.
+    * @param directory string that contains the directory that the file is put into.
+    */
    /*@ requires (* must be a valid schedule *)
      @ ensures (* must be a valid PDF file in the format that 
      @ peopleSoft expects *)
      @*/ 
    // creates a PDF of the schedule
-   public abstract void exportToPDF(Schedule schedule);
+   public abstract void exportToPDF(Schedule schedule, String directory);
    
 }
