ff6dffec44dda194cc10be86bb960af8a0664af2
cjgaydosh@gmail.com <cjgaydosh@gmail.com@5389afa3-3cff-bc22-2e1c-0e5405de9ceb>
2011-12-06 05:46:04 +0000

    updated publish tab gui

--- trunk/prototype/src/keycow/scheduler/view/PublishTab.java
+++ trunk/prototype/src/keycow/scheduler/view/PublishTab.java
@@ -1,8 +1,70 @@
 package keycow.scheduler.view;
 
-import javax.swing.*;
+import java.io.*;
 import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import javax.swing.SwingUtilities;
+import javax.swing.filechooser.*;
 
-public class PublishTab extends JPanel {
 
+
+public class PublishTab extends JPanel implements ActionListener{
+   private JPanel panel;
+   private JTextField fileNameField;
+   private JButton selectionButton;
+   private JButton exportButton;
+   private JFileChooser fChoose;
+    
+   public PublishTab()
+   {
+      panel = new JPanel();
+      add(panel);
+      
+      Box vbox = Box.createVerticalBox();
+      Box buttonBox = Box.createHorizontalBox();
+      //Box fileNameBox
+      
+      fChoose = new JFileChooser();
+      fileNameField = new JTextField();
+      fChoose.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+      
+      //selectionButton = new JButton("Change Directories");
+      exportButton = new JButton("Export");
+      
+      //buttonBox.add(selectionButton);
+      //buttonBox.add(Box.createHorizontalStrut(15));
+      buttonBox.add(exportButton);
+      
+      //selectionButton.addActionListener(this);
+      exportButton.addActionListener(this);
+      
+      
+      
+      vbox.add(buttonBox);
+      
+      
+      //vbox.add(fChoose);
+      
+      panel.add(vbox);
+      
+      
+      
+      
+   }
+
+   public void actionPerformed(ActionEvent e) {
+
+     //Handle open button action.
+     if (e.getSource() == exportButton) {
+         int returnVal = fChoose.showSaveDialog(PublishTab.this);
+
+         if (returnVal == JFileChooser.APPROVE_OPTION) {
+             File file = fChoose.getSelectedFile();
+             System.out.println("saving: " + file.getName() + ".\n");
+         } else {
+             System.out.println("Export command cancelled by user.\n");
+         }
+      }
+   }
 }
