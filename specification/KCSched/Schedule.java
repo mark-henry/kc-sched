eda03f22649e1884e03643dee7ff18c9a7a51366
TheRatcheteer@gmail.com <TheRatcheteer@gmail.com@5389afa3-3cff-bc22-2e1c-0e5405de9ceb>
2011-12-07 23:50:48 +0000

    Turned Schedule.java into a CRUD database.

--- specification/KCSched/Schedule.java
+++ specification/KCSched/Schedule.java
@@ -5,5 +5,32 @@ import java.util.*;
 /** This is a container for a schedule object. A Schedule is merely a collection of sections, with no conflicting
   * entries. See the GenerateSchedule class for a more complete view of schedule generation **/
 public abstract class Schedule {
-	Collection<Section> Sections;
+	Collection<Section> sections;
+	
+	//The Schedule document is a basic CRUD database of Sections.
+	
+	/*@
+	ensures
+	    sections.contains(sec);
+	@*/
+	public abstract void add(Section sec);
+	
+	/*@
+	requires
+	    sections.contains(sec);
+	ensures
+	    !sections.contains(sec);
+	@*/
+	public abstract void delete(Section sec);
+	
+	/*@
+	requires
+	    sections.contains(oldSec);
+    ensures
+	    !sections.contains(oldSec);
+		sections.contains(newSec);
+	@*/
+	public abstract void modify(Section oldSec, Section newSec);
+	
+	public abstract void read();
 }
