d832563fd9c9a89a7efdad315ed7336acdfcbc32
zealottemplar@gmail.com <zealottemplar@gmail.com@5389afa3-3cff-bc22-2e1c-0e5405de9ceb>
2011-12-07 21:40:51 +0000

    work on rroom  gui

--- prototype/src/keycow/scheduler/view/data/RoomsTab.java
+++ prototype/src/keycow/scheduler/view/data/RoomsTab.java
@@ -15,39 +15,27 @@ public class RoomsTab extends JPanel {
    
    public void createRoomList() {
       
-      /*
-       * This is the left of the two main windows
-       */
+
       room_list = new JPanel();
       room_list.setPreferredSize(new Dimension(400, 650));
       room_list.setLayout(new BoxLayout(room_list, BoxLayout.Y_AXIS));
       room_list.setBorder(new TitledBorder("Room List"));
       
-      /*
-       * Hard coded info
-       * Table holds all teacher info
-       */
+
       String columns[] = {"Building Number", "Room Number"};
       Object data[][] = {{"16", "256"}, {"16", "301"}, {"16", "302"}, {"16", "303"}};
       JTable table = new JTable(data, columns);
       
-      /*
-       * JPanel to hold the add / delete button
-       */
+
       JPanel button_panel = new JPanel();
       button_panel.setLayout(new BoxLayout(button_panel, BoxLayout.X_AXIS));
       button_panel.add(new JButton("+"));
       button_panel.add(new JButton("-"));
       
-      /*
-       * add the table and button panel to the left window
-       */
+
       room_list.add(table);
       room_list.add(button_panel);
       
-      /*
-       * add the window to the main frame
-       */
       
       add(room_list);
    }
@@ -57,41 +45,35 @@ public class RoomsTab extends JPanel {
       room_details.setPreferredSize(new Dimension(400, 650));
       room_details.setBorder(new TitledBorder("Room Details"));
       
-      /*
-       * hard-coded labels
-       */
+
       String labels[] = {"Building Number: ", "Room Number: ", "Capacity: ", 
-      	"Lab: ", "Stations: ", "Features: "};
+      	"Lab Room: ", "Stations: ", "Features: "};
       
-      /*
-       * construct 6 JPanels and add each label / textfield to it
-       * then add the row to the right window
-       */
+
       for (int i = 0; i < labels.length; i++) {
-         JPanel row = new JPanel();
-         row.setLayout(new BoxLayout(row, BoxLayout.X_AXIS));
-         row.add(new JLabel(labels[i], SwingConstants.LEFT));
-         
-         if (labels[i].equals("Lab: ")) 
+         JPanel row = new JPanel(new FlowLayout(FlowLayout.LEFT));
+          JLabel label = new JLabel(labels[i]);
+          label.setAlignmentX(Component.LEFT_ALIGNMENT);
+          row.add(label);
+          
+         if (labels[i].equals("Lab Room: ")) 
          	row.add(new JCheckBox());
+         else if (labels[i].equals("Features: "))
+             row.add(new JTextArea(10, 20));
          else
          	row.add(new JTextField(20));
          room_details.add(row);
+          row.setAlignmentX(Component.LEFT_ALIGNMENT);
       }
  
-      /*
-       * add buttons 
-       */
+
        JPanel row = new JPanel();
        row.setLayout(new BoxLayout(row, BoxLayout.X_AXIS));
        row.add(new JButton("Commit"));
        row.add(new JButton("Restore"));
        room_details.add(row);
 
-       
-      /*
-       * add the right window to the main frame
-       */
+
       add(room_details);
    }
 }
