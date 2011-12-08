509764b20a305709e33a661785ff6e790e60c363
boardsportsrule13@gmail.com <boardsportsrule13@gmail.com@5389afa3-3cff-bc22-2e1c-0e5405de9ceb>
2011-12-08 00:49:55 +0000

    Gui changes

--- prototype/src/keycow/scheduler/view/SchedulerUI.java
+++ prototype/src/keycow/scheduler/view/SchedulerUI.java
@@ -2,22 +2,14 @@ package keycow.scheduler.view;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.*;
 
 public class SchedulerUI extends JFrame {
     JTabbedPane tabbedPane = new JTabbedPane();
 
     public SchedulerUI() {
-       JMenuBar menuBar = new JMenuBar();
-       
-       JMenu file = new JMenu("File");
-       menuBar.add(file);
-       
-       JMenuItem save = new JMenuItem("Save");
-       menuBar.add(save);
-       
-       this.setJMenuBar(menuBar);
-       //this.add(menuBar); 
-       
+   
+       createMenuBar();
        createTabs();
 
         setMinimumSize(new Dimension(1024, 768));
@@ -35,5 +27,60 @@ public class SchedulerUI extends JFrame {
         tabbedPane.addTab(BigTabs.paddedTitle("Edit", 15, 8),     new EditTab());
         tabbedPane.addTab(BigTabs.paddedTitle("Publish", 15, 8),  new PublishTab());
     }
+	
+	public void createMenuBar(){   
+		JMenuBar menuBar = new JMenuBar();
+       JMenu dropDown = new JMenu("==");
+       menuBar.add(Box.createHorizontalGlue());
+	   menuBar.add(dropDown);
+	   JMenu file = new JMenu("File");
+	   file.add(new JMenuItem("New"));
+	   file.add(new JMenuItem("Open"));
+	   file.add(new JMenuItem("Save, Save As"));
+	   file.add(new JMenuItem("Export"));
+	   file.add(new JMenuItem("Print"));
+	   dropDown.add(file);
+	    JMenu edit = new JMenu("Edit");
+	   edit.add(new JMenuItem("Undo"));
+	   edit.add(new JMenuItem("Redo"));
+	   edit.add(new JMenuItem("Cut"));
+	   edit.add(new JMenuItem("Copy"));
+	   edit.add(new JMenuItem("Paste"));
+	   dropDown.add(edit);
+	   dropDown.addSeparator();
+	   JMenu view = new JMenu("View");
+	   dropDown.add(view);
+	   view.add(new JMenuItem("Data"));
+	   view.add(new JMenuItem("Generate"));
+	   view.add(new JMenuItem("Edit"));
+	   view.add(new JMenuItem("Publish"));
+		dropDown.addSeparator();
+		JMenuItem exit = new JMenuItem("Exit");
+		exit.addActionListener(new ActionListener() {
+         public void actionPerformed(ActionEvent e) {
+            int n =JOptionPane.showConfirmDialog(null, "Would you like to Exit?", "Confirm", JOptionPane.YES_NO_OPTION);
+         if(n == JOptionPane.NO_OPTION)
+			return;
+		System.exit(0);
+		 }
+      });
+		
+       
+	   
+		JMenuItem about = new JMenuItem("About");
+		about.addActionListener(new ActionListener() {
+         public void actionPerformed(ActionEvent e) {
+            JOptionPane.showMessageDialog(null, "This feature brought to you by Andy Crafts","About", JOptionPane.WARNING_MESSAGE);
+     
+		 }
+      });
+		dropDown.add(about);
+	   dropDown.add(exit);
+	   
+       this.setJMenuBar(menuBar);
+	   }
+	
+	
+	
     
 }
